dbd65a6618b514ba43657ca08138d2d22a9f343b
Marcos Coelho <marcoscoelho1@yahoo.com.br>
2024-05-02 15:35:35 -0300

    feat: create service to upload users avatar

---
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -0,0 +1,38 @@
+import { getCustomRepository } from 'typeorm';
+import { UsersRepository } from '../typeorm/repositories/UserRepository';
+import AppError from '@shared/errors/AppError';
+import path from 'path';
+import uploadConfig from '@config/upload';
+import fs from 'fs';
+
+interface IRequest {
+  user_id: string;
+  avatarFileName: string;
+}
+
+class UpdateUserAvatarService {
+  public async execute({ user_id, avatarFileName }: IRequest) {
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const user = await usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('User not found.');
+    }
+
+    if (user.avatar) {
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
+
+      user.avatar = avatarFileName;
+
+      await usersRepository.save(user);
+    }
+  }
+}
+
+export default UpdateUserAvatarService;
