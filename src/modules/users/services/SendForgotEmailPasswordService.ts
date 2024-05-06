91256bcaeb7ed70412f0025f1027608b60f6e78c
Marcos Coelho <marcoscoelho1@yahoo.com.br>
2024-05-06 17:44:03 -0300

    feat: create service to generate token

---
+++ src/modules/users/services/SendForgotEmailPasswordService.ts
@@ -0,0 +1,27 @@
+import { getCustomRepository } from 'typeorm';
+import { UsersRepository } from '../typeorm/repositories/UserRepository';
+import AppError from '@shared/errors/AppError';
+import { UserTokenRepository } from '../typeorm/repositories/UserTokenRepository';
+
+interface IRequest {
+  email: string;
+}
+
+class SendForgotEmailPasswordService {
+  public async execute({ email }: IRequest): Promise<void> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const userTokensRepository = getCustomRepository(UserTokenRepository);
+
+    const user = await usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('User does not exists.');
+    }
+
+    const token = await userTokensRepository.generate(user.id);
+
+    console.log('token', token);
+  }
+}
+
+export default SendForgotEmailPasswordService;
